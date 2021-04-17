a7467ae7f4ff757ed40f023beb7cbf1af901da3c
Patricia Aas <psmaas@gmail.com>
2021-04-17 17:52:49 +0200

    Reformat code files

--- src/heartbleed/heartbleed.c
+++ src/heartbleed/heartbleed.c
@@ -17,30 +17,29 @@
  * https://www.openssl.org/source/license-openssl-ssleay.txt
  */
 
-#define TLS1_RT_HEARTBEAT    24
-#define TLS1_HB_REQUEST    1
-#define TLS1_HB_RESPONSE  2
+#define TLS1_RT_HEARTBEAT 24
+#define TLS1_HB_REQUEST 1
+#define TLS1_HB_RESPONSE 2
 
-#define n2s(c, s)  ((s=(((unsigned int)(c[0]))<< 8)| \
-          (((unsigned int)(c[1]))    )),c+=2)
-#define s2n(s, c)  ((c[0]=(unsigned char)(((s)>> 8)&0xff), \
-        c[1]=(unsigned char)(((s)    )&0xff)),c+=2)
+#define n2s(c, s) ((s = (((unsigned int)(c[0])) << 8) | (((unsigned int)(c[1])))), c += 2)
+#define s2n(s, c) ((c[0] = (unsigned char)(((s) >> 8) & 0xff), c[1] = (unsigned char)(((s)) & 0xff)), c += 2)
 
-typedef struct RRec {
+typedef struct RRec
+{
   unsigned char *data;
   int length;
 } RRec;
 
-typedef struct S3 {
+typedef struct S3
+{
   RRec rrec;
 } S3;
 
-typedef struct SSL {
+typedef struct SSL
+{
   S3 *s3;
 
-  void
-  (*msg_callback)(int write_p, int version, int content_type, const void *buf,
-                  size_t len, void *ssl, void *arg);
+  void (*msg_callback)(int write_p, int version, int content_type, const void *buf, size_t len, void *ssl, void *arg);
 
   void *msg_callback_arg;
   int version;
@@ -48,25 +47,29 @@ typedef struct SSL {
   unsigned int tlsext_hb_pending;
 } SSL;
 
-unsigned char *OPENSSL_malloc(unsigned int i) {
+unsigned char *OPENSSL_malloc(unsigned int i)
+{
   return malloc(i);
 }
 
-void OPENSSL_free(unsigned char *buffer) {
+void OPENSSL_free(unsigned char *buffer)
+{
   free(buffer);
 }
 
 int RAND_pseudo_bytes(unsigned char *buf, int num) { return 42; }
 void dtls1_stop_timer(SSL *pSsl) {}
 
-int dtls1_write_bytes(SSL *s, int type, unsigned char *buf, int len, char ** response) {
-  char * response_buf = malloc(len);
+int dtls1_write_bytes(SSL *s, int type, unsigned char *buf, int len, char **response)
+{
+  char *response_buf = malloc(len);
   memcpy(response_buf, buf, len);
   *response = response_buf;
   return len;
 }
 
-int dtls1_process_heartbeat(SSL *s, char ** response) {
+int dtls1_process_heartbeat(SSL *s, char **response)
+{
   unsigned char *p = &s->s3->rrec.data[0], *pl;
   unsigned short hbtype;
   unsigned int payload;
@@ -78,9 +81,7 @@ int dtls1_process_heartbeat(SSL *s, char ** response) {
   pl = p;
 
   if (s->msg_callback)
-    s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT,
-                    &s->s3->rrec.data[0], s->s3->rrec.length,
-                    s, s->msg_callback_arg);
+    s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT, &s->s3->rrec.data[0], s->s3->rrec.length, s, s->msg_callback_arg);
 
   if (hbtype == TLS1_HB_REQUEST) {
     unsigned char *buffer, *bp;
@@ -104,9 +105,7 @@ int dtls1_process_heartbeat(SSL *s, char ** response) {
     r = dtls1_write_bytes(s, TLS1_RT_HEARTBEAT, buffer, 3 + payload + padding, response);
 
     if (r >= 0 && s->msg_callback)
-      s->msg_callback(1, s->version, TLS1_RT_HEARTBEAT,
-                      buffer, 3 + payload + padding,
-                      s, s->msg_callback_arg);
+      s->msg_callback(1, s->version, TLS1_RT_HEARTBEAT, buffer, 3 + payload + padding, s, s->msg_callback_arg);
 
     OPENSSL_free(buffer);
 
@@ -130,7 +129,8 @@ int dtls1_process_heartbeat(SSL *s, char ** response) {
   return 0;
 }
 
-int heartbleed(unsigned char * request, int len, char ** response) {
+int heartbleed(unsigned char *request, int len, char **response)
+{
   SSL s;
   s.s3 = malloc(sizeof(S3));
   s.s3->rrec.data = request;
--- test/catch_main/catch_main.cpp
+++ test/catch_main/catch_main.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN // This tells the catch header to generate a main
+// This tells the catch header to generate a main
+#define CATCH_CONFIG_MAIN
 
 #include <catch2/catch.hpp>
-
-
