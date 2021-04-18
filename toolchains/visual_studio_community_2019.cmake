8a62c9c3cce67b5e63f91ee0840eb4090b550313
Patricia Aas <patricia@turtlesec.no>
2021-04-18 17:34:45 +0200

    Specify compiler for Windows: Visual Studio Community 2019

---
+++ toolchains/visual_studio_community_2019.cmake
@@ -0,0 +1,2 @@
+SET(CMAKE_C_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.28.29910/bin/Hostx86/x86/cl.exe")
+SET(CMAKE_CXX_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.28.29910/bin/Hostx86/x86/cl.exe")
