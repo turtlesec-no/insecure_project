1b34af606faf99eb446d33e5ac9be2b4579c2d19
Patricia Aas <patricia@turtlesec.com>
2021-04-18 11:57:12 +0200

    Upgrade catch2

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -14,7 +14,7 @@ macro(run_conan)
     conan_cmake_run(
             REQUIRES
             ${CONAN_EXTRA_REQUIRES}
-            catch2/2.13.3
+            catch2/2.13.6
             docopt.cpp/0.6.2
             fmt/6.2.0
             spdlog/1.5.0
